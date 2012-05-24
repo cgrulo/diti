30d7a2c2c1b6df185ef47f5c79c79382cfc9d588
charly <charly@Acer.(none)>
2012-05-24 15:40:32 -0500

    Anadido Modelos Producto y Categorías

--- main/models.py
+++ main/models.py
@@ -34,6 +34,30 @@ class Section(models.Model):
     def __unicode__ (self):
         return self.title
 
+class BlogPost(models.Model):
+    title = models.CharField(max_length=100)
+    slug = models.SlugField()
+    imagen = models.ImageField(upload_to='img/')
+    
+# Productos
+
+class Product(models.Model):
+    title = models.CharField(max_length=100)
+    slug = models.SlugField()
+    imagen = models.ImageField(upload_to='img/')
+    resumen = models.TextField()
+    ficha_tecnica = models.FileField(upload_to='files/')
+    categoria = models.ForeignKey('main.ProductCategory')
+    
+    def __unicode__(self):
+        return self.title
+    
+class ProductCategory(models.Model):
+    title = models.CharField(max_length=100)
+    slug = models.SlugField()
+    
+    def __unicode__(self):
+        return self.title
    
 class ContactForm(forms.Form):
     nombre = forms.CharField()
